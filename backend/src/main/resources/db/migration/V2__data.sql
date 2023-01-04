d5db29e15d813bd63ae66429ef1cfa258ba23eed
MariiaS3 <mariia.sydor28082001@gmail.com>
2023-01-04 20:04:37 +0100

    ...

--- backend/src/main/resources/db/migration/V2__data.sql
+++
@@ -1,2 +0,0 @@
-insert into dirorfile(id ,path ,name  ,link  ,permisions ,username  ,groupname ,is_directory ,size ,text ,time ) values (3,'/', 'dir', 2, 'drwxr-xr-x','root', 'group','true', 4096, '', 'Aug 7 10:51');
-insert into dirorfile(id ,path ,name  ,link  ,permisions ,username  ,groupname ,is_directory ,size ,text ,time ) values (4,'/dir', 'file.txt', 1, '-rw-r--r--','root', 'group','false', 4096, 'some string', 'Aug 8 11:51');
--- backend/src/test/resources/InsertInitialDirorfileRecordForTest.sql
+++
@@ -1,2 +0,0 @@
-insert into dirorfile(id ,path ,name  ,link  ,permisions ,username  ,groupname ,is_directory ,size ,text ,time ) values (1,'/testfolder', 'testdir', 2, 'drwxr-xr-x','root', 'group','true', 4096, '', 'Aug 7 10:51');
-insert into dirorfile(id ,path ,name  ,link  ,permisions ,username  ,groupname ,is_directory ,size ,text ,time ) values (2,'/testfolder', 'testfile.txt', 1, '-rw-r--r--','root', 'group','false', 4096, 'some string', 'Aug 8 11:51');
