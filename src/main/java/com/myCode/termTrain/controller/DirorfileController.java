340f6eb47950993418781d7227bc56d3472a0958
MariiaS3 <mariia.sydor28082001@gmail.com>
2023-01-04 19:33:06 +0100

    circleci

--- src/main/java/com/myCode/termTrain/controller/DirorfileController.java
+++ src/main/java/com/myCode/termTrain/controller/DirorfileController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,14 @@ public class DirorfileController {
         this.dirFileService = dirFileService;
     }
 
+    @ApiOperation(value = "add new file/dir", response =  DirorfileDto[].class, produces = "aplication/json")  //about this endpoint
+    @ApiResponses(value = {
+        @ApiResponse(code = 200 , message = "Succesfully added file/dir"),
+        @ApiResponse(code = 403 , message = "Accessing the resource you were trying to reach is forbidden"),
+        @ApiResponse(code = 404, message = "not found resource")
+
+    })
+    @PostMapping("/add-new")
     public ResponseEntity<?> addNewDirOrFile(@RequestBody Dirorfile dirFile){
         Dirorfile dirOrFile = dirFileService.addNewDirOrFile(dirFile);
         return new ResponseEntity<>(dirOrFile, HttpStatus.CREATED);
